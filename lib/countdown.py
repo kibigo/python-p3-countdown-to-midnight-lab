e6364a2f6fc252bb3388afa54e860dd3ddedb2d2
kibigo <jacksonkibigo99@gmail.com>
2023-10-03 20:02:06 +0300

    Completed Assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
 # your code goes here!
+import time
+
+x=5
+def countdown(x):
+    while x >0:
+        print(f"{x} SECOND(S)!")
+        if x == 1:
+            print("HAPPY NEW YEAR!")
+        x -=1
+
+countdown(x)
+
+def countdown_with_sleep(x):
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        time.sleep(1)
+        if x == 1:
+            print("HAPPY NEW YEAR!")
+        x -=1
+countdown_with_sleep(x)
